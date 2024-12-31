8a49319fbc585e0fa5e78e75fb2be01397da3afc
lumen <lumen2004.scholar@gmail.com>
2024-12-31 20:16:29 +0800

    Perf: Deprecate ContextDatasetWithSynQA, add syn QA template to ContextDataset.

--- lift/context_dataset.py
+++ lift/context_dataset.py
@@ -99,112 +99,12 @@ class ContextDataset(Dataset):
     
     def __getitem__(self, index):
         return self.preprocessing(self.data[index])
-
-
-class DatasetWithSyntheticQA(ContextDataset):
-    def __init__(self, context: str, tokenizer: PreTrainedTokenizer, model_max_length: int=4096, block_size: int=256, len_segment: int=8, len_offset: int=3, num_generate_qa: int=0, generator_name_or_path: Optional[str]=None, title: Optional[str]=None):
-        super().__init__(context, tokenizer, model_max_length, block_size, len_segment, len_offset)
-        # Load the generator
-        quantization_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_compute_dtype=torch.bfloat16,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type='nf4'
-        )
-        generator = AutoModelForCausalLM.from_pretrained(
-            generator_name_or_path,
-            trust_remote_code=True,
-            device_map="auto",
-            torch_dtype = torch.bfloat16,
-            quantization_config=quantization_config,
-        )
-        # Generate synthetic QAs
-        self.num_generate_qa = num_generate_qa
-        temp_qas = self.shortqa_gen(generator, context, num_generate_qa)
-        torch.cuda.empty_cache()
-        for qa in temp_qas:
-            user_msg, assistant_msg = apply_qa_template(
-                question=qa['Q'],
-                answer=qa['A'],
-                evidences=qa['S'],
-                title=title,
-                context=context,
-                return_answer=True
-            )
-            messages = [
-                {'role': 'system', 'content': "You are a helpful assistant."},
-                {'role': 'user', 'content': user_msg},
-                {'role': 'assistant', 'content': assistant_msg}
-            ]
-            input_length = len(self.tokenizer.apply_chat_template(messages[:-1], add_generation_prompt=True))
-            input_ids = self.tokenizer.apply_chat_template(messages, add_generation_prompt=False)
-            output_length = len(input_ids) - input_length
-            if len(input_ids) > model_max_length:
-                input_ids = input_ids[:model_max_length//2] + input_ids[-model_max_length//2:]
-                input_length = len(input_ids) - output_length
-            self.data.append((input_ids, input_length))
-        # Set tag
-        self.enable_syn_qa = False
-
-    def __len__(self):
-        return self.num_segments + self.num_generate_qa if self.enable_syn_qa else self.num_segments
-
-    @torch.no_grad()
-    def shortqa_gen(self, generator, full_context: str, num_generate_qa: int=0):
-        generated = []
-        texts = sent_tokenize(full_context)
-        c = 0
-        assert len(texts) >= 25
-        for _ in tqdm.tqdm(range(num_generate_qa), desc="Generate QA"):
-            st_pos = randint(0, len(texts) - 25)
-            context = ' '.join(texts[st_pos:st_pos+25])
-            messages = [
-                {
-                    'role': "system",
-                    'content': "You are a helpful assistant."
-                },
-                {
-                    'role': "user", 
-                    'content': f"You are given a piece of text as the context. You should generate ONLY one question and the corresponding answer according to the context. You should also select one or more original sentences in the context as the evidences. Please answer in the following format:\nQuestion: [question]\nAnswer: [answer]\nEvidence:\n- [evidence 1]\n- [evidence 2]\n...\nPlease DON'T output quotes when outputing evidences. The following is the piece of text: {context}"
-                }
-            ]
-            input_ids = self.tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt")
-            mask_attention = torch.ones_like(input_ids)
-            terminators = [self.tokenizer.eos_token_id, self.tokenizer.convert_tokens_to_ids("<|eot_id|>")]
-            num_of_trials = 0
-            while True:
-                outputs = generator.generate(
-                    input_ids.to(generator.device),
-                    max_new_tokens=1024,
-                    attention_mask=mask_attention.to(generator.device),
-                    pad_token_id=self.tokenizer.eos_token_id,
-                    eos_token_id=terminators,
-                    do_sample=False,
-                )
-                response = self.tokenizer.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
-                question_position = response.find("Question:")
-                answer_position = response.find("Answer:")
-                evidence_position = response.find("Evidence:")
-                question = response[question_position + 9:answer_position].strip()
-                answer = response[answer_position + 7:evidence_position].strip()
-                evidences = response[evidence_position + 9:].strip().split('\n')
-                evidences = list(map(lambda s: s[s.find('-') + 2:].strip(), evidences))
-                c += 1
-                if question_position == -1 or answer_position == -1 or evidence_position == -1:
-                    num_of_trials += 1
-                    if num_of_trials > 5:
-                        break
-                    continue
-                else:
-                    question = response[question_position+9:answer_position].strip()
-                    answer = response[answer_position+7:evidence_position].strip()
-                    evidence = response[evidence_position+9:].strip()
-                    generated.append({"Q":question, "A":answer, "S":evidence})
-                    break
-        return generated
-
+    
     def enable_qa(self):
-        self.enable_syn_qa = True
-        
+        raise NotImplementedError
+    
     def disable_qa(self):
-        self.enable_syn_qa = False
+        raise NotImplementedError
+    
+    def generate_task(self):
+        raise NotImplementedError
--- lift/train.py
+++ lift/train.py
@@ -49,25 +49,19 @@ def load_trainer(model: PreTrainedModel, training_dataset: Dataset, tokenizer: P
 def train(model: PreTrainedModel, dataset: ContextDataset, tokenizer: PreTrainedTokenizer, training_args: TrainingArguments, involve_qa_epochs: int=0, gather_batches: bool=True):
     """Fine-tune the model and the corresponding tokenizer.
     Args:
-        dataset (Dataset): the dataset to train on.
-        tokenizer (PreTrainedTokenizer): a Llama tokenizer (or other tokenizers with chat template).
-        training_args (TrainingArguments): transformers-style training arguments, used for the trainer.
-        gather_batches (bool): OPTIONAL, default to `False`; if `gather_batches=True`, it will force the trainer to update the model only once every epoch; it may lead to more stable gradients.
-        use_lora (bool): OPTIONAL, default to `False`; whether to use LoRA.
-        lora_rank (int): OPTIONAL, default to `None`; assign it when `use_lora=True`.
-        full_ft (bool): OPTIONAL, default to `False`; whether to full-fine-tune the model.
-        load_in_4bit (bool): OPTIONAL, default to `False`; it must be used with `use_lora=True`.
-        load_in_8bit (bool): OPTIONAL, default to `False`; it must be used with `use_lora=True`.
-        cache_dir (str): OPTIONAL, default to `None`.
-        model_revision (str): OPTIONAL, default to `"main"`.
-        use_auth_token (bool): OPTIONAL, default to `False`.
+        model (PreTrainedModel): the model to fine-tune.
+        dataset (ContextDataset): the dataset for fine-tuning.
+        tokenizer (PreTrainedTokenizer): the pretrained tokenizer.
+        training_args (TrainingArguments): the huggingface training arguments.
+        involve_qa_epochs (int): OPTIONAL, default to `0`; the number of epochs to involve QA pairs.
+        gather_batches (bool): OPTIONAL, default to `True`; if `gather_batches=True`, it will force the trainer to update the model only once every epoch; it may lead to more stable gradients.
     Returns:
         model_tokenizer_pair (tuple[PreTrainedModel, PreTrainedTokenizer]): the fine-tuned model and the corresponding tokenizer.
     """
     # load tokenzier
     torch.cuda.empty_cache()  # Manually release memory
     # Load and finetune the model
-    if isinstance(dataset, DatasetWithSyntheticQA):
+    if involve_qa_epochs > 0:
         dataset.disable_qa()
     trainer, model = load_trainer(
         model=model,
@@ -80,8 +74,7 @@ def train(model: PreTrainedModel, dataset: ContextDataset, tokenizer: PreTrained
         trainer.train()
     # Load the dataset with QA pairs and continue-finetune the model
     if involve_qa_epochs > 0:
-        if isinstance(dataset, DatasetWithSyntheticQA):
-            dataset.enable_qa()
+        dataset.enable_qa()
         training_args_syn = deepcopy(training_args)
         training_args_syn.num_train_epochs = involve_qa_epochs
         trainer_syn, model = load_trainer(
