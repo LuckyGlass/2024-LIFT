090fbd0176ec7715415ec8b8d669b46611d809c3
Xi-yuanWang <wangxiyuan@pku.edu.cn>
2025-01-08 06:31:22 +0000

    fixGM

--- lift/gated_memory/utils.py
+++ lift/gated_memory/utils.py
@@ -13,12 +13,11 @@ def preprocess(model_name_or_path: str, output_dir: str):
     model_config = AutoConfig.from_pretrained(model_name_or_path, local_files_only=True)
     model = GMLlamaForCausalLM.from_pretrained(model_name_or_path, config=model_config, local_files_only=True, low_cpu_mem_usage=False, _fast_init=False)
     lora_config = LoraConfig(
-        r=4,
+        r=2,
         target_modules=["lm_head"],
         task_type=TaskType.CAUSAL_LM,
-        lora_alpha=8,
-        lora_dropout=0.05,
-        modules_to_save=[f"layers.{i}.self_attn.mem_proj" for i in range(32)]+[f"layers.{i}.self_attn.gate_proj" for i in range(32)],
+        lora_alpha=4,
+        modules_to_save=[f"layers.{i}.self_attn.mem_proj" for i in range(len(model.model.layers))]+[f"layers.{i}.self_attn.gate_proj" for i in range(len(model.model.layers))],
     )
     model = get_peft_model(model, lora_config)
     model.save_pretrained(output_dir)
